7638fa0fb95314cd2c4061d5999018d686339c10
Nicolas Contreras <nicolascontreras677@gmail.com>
2026-06-04 12:19:31 -0300

    Release v2.9
    - Bump versionCode: 7 → 8
    - Bump versionName: 2.8 → 2.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.myapplication"
         minSdk = 31
         targetSdk = 35
-        versionCode = 7
-        versionName = "2.8"
+        versionCode = 8
+        versionName = "2.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
